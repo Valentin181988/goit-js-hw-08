4cf38074d6dbedda7aba8098ef8a03ee86409e0d
Valentin Sachenko <valentin181988@gmail.com>
2021-10-27 19:12:08 +0300

    task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash/throttle';
 
+const form = document.querySelector(".feedback-form");
+const messageInput = document.querySelector('[name="message"]');
+const emailInput = document.querySelector('[name="email"]');
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+
+fillFormAfterReload();
+
+const throttleFunction = throttle(() => {
+     const data = {
+        message: messageInput.value,
+        email: emailInput.value
+     };
+    
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data));
+    console.log('throttle')
+}, 500);
+
+form.addEventListener('input', () => {
+
+    throttleFunction();
+});
+
+form.addEventListener("submit", (evt) => {
+     evt.preventDefault();
+    
+    const savedInput = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    
+    localStorage.clear();
+    form.reset();
+
+    console.log(savedInput);
+});
+
+function fillFormAfterReload() {
+    const savedInput = localStorage.getItem(LOCALSTORAGE_KEY);
+
+    if (savedInput) {
+        const parsedInput = JSON.parse(savedInput);
+        
+        messageInput.value = parsedInput.message;
+        emailInput.value = parsedInput.email;
+    };
+}
